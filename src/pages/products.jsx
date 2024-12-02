ed924c3b7b9bf8120e25afe0adaf100703e64b17
venkataniharbillakurthi <venkataniharbillakurthi@gmail.com>
2024-12-02 14:20:55 +0530

    Initial commit

---
+++ src/pages/products.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+function Products() {
+  return (
+    <div>
+
+      <h2 style={{ margin: '40px 20px' }}>Products</h2> 
+      <div  id="container">
+       
+      </div>
+
+    </div>
+  );
+}
+
+export default Products;
